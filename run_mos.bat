fe4dc37ced345a15d02aef36da10b16826e27e34
fsmosca <fsmosca@users.noreply.github.com>
2017-03-28 01:52:51 +0800

    Update run_mos.bat

--- run_mos.bat
+++ run_mos.bat
@@ -1,3 +1,6 @@
 move_ordering_spy.py --engine "Sf8.exe" --hash 128 --threads 1  --epd "STS1-STS15_LAN_v3.epd" --depth 12 --logging 1
 
+:: If using exe file in windows
+:: move_ordering_spy.exe --engine "Sf8.exe" --hash 128 --threads 1  --epd "STS1-STS15_LAN_v3.epd" --depth 12 --logging 1
+
 pause
